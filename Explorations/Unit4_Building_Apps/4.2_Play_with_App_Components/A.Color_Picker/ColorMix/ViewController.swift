17ced0d9e80093674bc794d44bde1faab0f539fe
Dahlia <down022@gmail.com>
2023-07-25 16:36:35 +0900

    Started Color Picker Project, putting a view onscreen to display a color

---
+++ Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/A.Color_Picker/ColorMix/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by 곽다은 on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        colorView.backgroundColor = .black
+    }
+
+
+}
+
