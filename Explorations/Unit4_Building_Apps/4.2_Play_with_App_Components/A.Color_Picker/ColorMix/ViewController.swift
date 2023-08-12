90ed818e751e8d7f9583a90cc7d0085fde0f3aea
Dahlia <down022@gmail.com>
2023-08-12 23:30:05 +0900

    Assign functionality to each switch

--- Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/A.Color_Picker/ColorMix/ViewController.swift
+++ Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/A.Color_Picker/ColorMix/ViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         colorView.backgroundColor = .black
     }
     
-    @IBAction func redSwitch(_ sender: UISwitch) {
-    }
     
-    @IBAction func greenSwitch(_ sender: UISwitch) {
-    }
-    
-    @IBAction func blueSwitch(_ sender: UISwitch) {
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = 1
+        } else if greenSwitch.isOn {
+            green = 1
+        } else if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
     }
 }
