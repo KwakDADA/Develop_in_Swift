b557367506059f6927f1db98f26ffbbe82024f6f
Dahlia <down022@gmail.com>
2023-08-13 18:25:23 +0900

    Add Slider to control ther intensity of each color

--- Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/A.Color_Picker/ColorMix/ViewController.swift
+++ Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/A.Color_Picker/ColorMix/ViewController.swift
@@ -13,14 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSwitch: UISwitch!
     @IBOutlet weak var greenSwitch: UISwitch!
     @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        colorView.backgroundColor = .black
+        updateColor()
     }
     
     @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
         updateColor()
     }
     
@@ -30,16 +37,17 @@ class ViewController: UIViewController {
         var blue: CGFloat = 0
         
         if redSwitch.isOn {
-            red = 1
+            red = CGFloat(redSlider.value)
         }
         if greenSwitch.isOn {
-            green = 1
+            green = CGFloat(greenSlider.value)
         }
         if blueSwitch.isOn {
-            blue = 1
+            blue = CGFloat(blueSlider.value)
         }
         
         let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
         colorView.backgroundColor = color
     }
+    
 }
