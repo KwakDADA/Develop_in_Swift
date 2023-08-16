7d4b739bc4acdc08065af511a73c9a4f9a65973e
Dahlia <down022@gmail.com>
2023-08-16 23:54:14 +0900

    Made messageCount a computed property and modified the openingLine to be visible when the app is first launched

--- Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/B.ChatBot/ChatBot/ChatBot/ConversationDataSource.swift
+++ Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/B.ChatBot/ChatBot/ChatBot/ConversationDataSource.swift
@@ -2,14 +2,15 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    var messageCount = 0
+    var messageCount: Int {
+        return messages.count
+    }
     
-    var messages = [Message]()
+    var messages = [openingLine]
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
-        messageCount += 1
         let message = Message(date: Date(), text: question, type: .question)
         messages.append(message)
     }
@@ -17,7 +18,6 @@ class ConversationDataSource {
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
-        messageCount += 1
         let message = Message(date: Date(), text: answer, type: .answer)
         messages.append(message)
     }
