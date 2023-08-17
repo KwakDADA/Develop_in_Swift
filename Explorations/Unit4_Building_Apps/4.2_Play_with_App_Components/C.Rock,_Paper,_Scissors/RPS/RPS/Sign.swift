6b74da8c3365d91528e0bbf499cd5b9f50d75fa3
Dahlia <down022@gmail.com>
2023-08-17 11:22:40 +0900

    Generated a random sign method

--- Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/C.Rock,_Paper,_Scissors/RPS/RPS/Sign.swift
+++ Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/C.Rock,_Paper,_Scissors/RPS/RPS/Sign.swift
@@ -18,17 +18,30 @@ enum Sign {
         }
     }
     
-    func compete(opponent opp: Sign) -> GameState {
+    func defeat(opponent opp: Sign) -> GameState {
         switch (self, opp) {
         case (.rock, .rock): return .draw
         case (.rock, .paper): return .lose
         case (.rock, .scissors): return .win
+            
         case (.paper, .rock): return .win
         case (.paper, .paper): return .draw
         case (.paper, .scissors): return .lose
+            
         case (.scissors, .rock): return .lose
         case (.scissors, .paper): return .win
         case (.scissors, .scissors): return .draw
         }
+    }
+}
+
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
     }
 }
