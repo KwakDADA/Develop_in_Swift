f66b7e0bf498c963acc85069d05caf64d032b43e
Dahlia <down022@gmail.com>
2023-08-27 19:25:02 +0900

    Started MemeMaker Project: Built the UI and made connections

---
+++ Explorations/Unit4_Building_Apps/4.2_Play_with_App_Components/D.MemeMaker/MemeMaker/MemeMaker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by 곽다은 on 2023/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    @IBOutlet weak var bottomCaptionLabel: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func valueChanged(_ sender: UISegmentedControl) {
+    }
+    
+
+}
+
