8b8787214ab3908bd67187cd310c885b3e042409
Dahlia <down022@gmail.com>
2023-09-14 14:25:59 +0900

    lesson 1.6 completed

---
+++ Fundamentals/Unit1_Getting_Started_with_App_Development/1.6_Xcode/LearningXcode/LearningXcode/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LearningXcode
+//
+//  Created by 곽다은 on 2023/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
