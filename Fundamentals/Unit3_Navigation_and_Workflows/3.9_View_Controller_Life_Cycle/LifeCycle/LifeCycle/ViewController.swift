bd163c3a67ca3107234a1f28f63735fbc804c8e9
Dahlia <down022@gmail.com>
2023-12-08 14:23:44 +0900

    fin

---
+++ Fundamentals/Unit3_Navigation_and_Workflows/3.9_View_Controller_Life_Cycle/LifeCycle/LifeCycle/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by 곽다은 on 12/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewController - View Did Load")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController - View Will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController - View Did Appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ViewController - View Will Disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController - View Did Disappear")
+    }
+}
