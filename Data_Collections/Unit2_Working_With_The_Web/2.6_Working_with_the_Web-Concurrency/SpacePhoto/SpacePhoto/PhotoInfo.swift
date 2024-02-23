9ecd31874adc52eccd2b725df1aad7ea88a9dfeb
Dahlia <down022@gmail.com>
2024-02-23 16:22:48 +0900

    fin: Lesson2.6

---
+++ Data_Collections/Unit2_Working_With_The_Web/2.6_Working_with_the_Web-Concurrency/SpacePhoto/SpacePhoto/PhotoInfo.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoInfo.swift
+//  SpacePhoto
+//
+//  Created by 곽다은 on 2/22/24.
+//
+
+import Foundation
+
+struct PhotoInfo: Codable {
+    var title: String
+    var description: String
+    var url: URL
+    var copyright: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description = "explanation"
+        case url
+        case copyright
+    }
+}
