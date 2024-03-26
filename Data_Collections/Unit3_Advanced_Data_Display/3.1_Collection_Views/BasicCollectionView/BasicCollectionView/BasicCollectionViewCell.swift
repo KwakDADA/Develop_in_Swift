2ea01b259118b2cd4c28bdf523beafa577ea2e74
KwakDADA <down022@gmail.com>
2024-03-26 14:09:52 +0900

    Lesson 3.1 Collection Views

---
+++ Data_Collections/Unit3_Advanced_Data_Display/3.1_Collection_Views/BasicCollectionView/BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by 곽다은 on 2/24/24.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var label: UILabel!
+    
+}
