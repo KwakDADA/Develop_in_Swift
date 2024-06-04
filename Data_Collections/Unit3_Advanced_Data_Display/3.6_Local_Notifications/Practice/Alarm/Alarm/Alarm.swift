46ccd5139908f3bb4f04195df7d04fea2bea0a4e
KwakDADA <down022@gmail.com>
2024-06-04 18:02:23 +0900

    Practice: create and schedule notifications

--- Data_Collections/Unit3_Advanced_Data_Display/3.6_Local_Notifications/Practice/Alarm/Alarm/Alarm.swift
+++ Data_Collections/Unit3_Advanced_Data_Display/3.6_Local_Notifications/Practice/Alarm/Alarm/Alarm.swift
@@ -12,13 +12,50 @@ import Foundation
 struct Alarm {
     static let notificationCategoryID = "AlarmNotification"
     static let snoozeActionID = "snooze"
+    private var notificationID: String
+    
+    var date: Date
+    
+    init(date: Date, notificationID: String? = nil) {
+        self.notificationID = notificationID ?? UUID().uuidString
+        self.date = date
+    }
     
     func schedule(completion: @escaping (Bool) -> ()) {
-        
+        authorizeIfNeeded { granted in
+            guard granted else {
+                DispatchQueue.main.async {
+                    completion(false)
+                }
+                return
+            }
+            
+            let content = UNMutableNotificationContent()
+            content.title = "Alarm"
+            content.body = "Beep Beep"
+            content.sound = UNNotificationSound.default
+            content.categoryIdentifier = Alarm.notificationCategoryID
+            
+            let triggerDateComponents = Calendar.current.dateComponents([.minute, .hour, .day, .month, .year], from: self.date)
+            let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDateComponents, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: self.notificationID, content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request) { (error: Error?) in
+                DispatchQueue.main.async {
+                    if let error = error {
+                        print(error.localizedDescription)
+                        completion(false)
+                    } else {
+                        completion(true)
+                    }
+                }
+            }
+        }
     }
     
     func unschedule() {
-        
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [notificationID])
     }
     
     /// this method should get the notification settings from the shared instance of UNUserNotificationCenter, then check the authorization status.
