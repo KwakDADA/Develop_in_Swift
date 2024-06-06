03bb582f81ddd26c422d6b7ea8b9c63e10f36c79
KwakDADA <down022@gmail.com>
2024-06-06 15:45:18 +0900

    Lesson 3.6 Lab: Request Authorization and Set Up the Notification Actions and Category

---
+++ Data_Collections/Unit3_Advanced_Data_Display/3.6_Local_Notifications/Lab/BillManager/BillManager/Model/Bill+Extras.swift
@@ -0,0 +1,59 @@
+//
+//  Bill+Extras.swift
+//  BillManager
+//
+
+import Foundation
+import NotificationCenter
+
+extension Bill {
+    static let notificationCategoryID = "BillNotification"
+    static let remindAgainActionID = "remindAgain"
+    static let markAsPaidActionID = "markAsPaid"
+    
+    var hasReminder: Bool {
+        return (remindDate != nil)
+    }
+    
+    var isPaid: Bool {
+        return (paidDate != nil)
+    }
+    
+    var formattedDueDate: String {
+        let dateString: String
+        
+        if let dueDate = self.dueDate {
+            dateString = dueDate.formatted(date: .numeric, time: .omitted)
+        } else {
+            dateString = ""
+        }
+        
+        return dateString
+    }
+    
+    func schedule(_ date: Date, completion: @escaping (Bool) -> ()) {
+        
+    }
+    
+    func unschedule() {
+        
+    }
+    
+    private func authorizeIfNeeded(completion: @escaping (Bool) -> ()) {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .notDetermined:
+                notificationCenter.requestAuthorization(options: [.alert, .sound]) { granted, _ in
+                    completion(granted)
+                }
+            case .denied, .provisional, .ephemeral:
+                completion(false)
+            case .authorized:
+                completion(true)
+            @unknown default:
+                completion(false)
+            }
+        }
+    }
+}
